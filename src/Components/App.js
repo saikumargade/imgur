c0d94ea60b4c4dc08df96b56adbb552cb81371ff
saikumargade <gade.saikumar1@gmail.com>
2019-09-30 19:12:13 +0530

    initial commit

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,11 +2,17 @@ import React from 'react';
 import '../App.css';
 import Post from './Post'
 
+// const images =['123','12345','abc'];
 function App() {
   return (
     <div className="App">
       <Post />
       <Post />
+      <Post />
+
+      {/* {
+        images.map(img => {return <Post title={img}/>})
+      } */}
     </div>
   );
 }
--- src/Components/Post.js
+++ src/Components/Post.js
@@ -2,11 +2,22 @@ import React from 'react'
 import logo from '../logo.svg'
 import '../App.css'
 export default class Post extends React.Component{
+  
     render(){
+        // const {title} = this.props
         return(
             <div className="post">
-                <img src={logo} alt="react logo"/>
+                <img width='100%' src={logo} alt="react logo"/>
+                {/* <p>{title}</p> */}
+                <div className='post-footer'>
+                <div className='upvote'></div>
+                <div className='downvote'></div>
+                <div className='icons'>
+                    <div className='up-count'>0</div>
+                    <div className='down-count'>0</div>
+                </div>
                 
+                </div>
             </div>
         )
     }
