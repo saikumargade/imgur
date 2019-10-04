971ed76765abf56899ce55ef01765804b4f51711
saikumargade <gade.saikumar1@gmail.com>
2019-10-04 12:43:32 +0530

    used grid for body component

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Post from './Post'
-const images =['description','123','abc','description','123','abc','description','123','abc','description','123','abc','new','description','123','abc','new','description','123','abc'];
+const images =['description','123','abc','description','123','abc','description','123','abc','description','abc','new','description','123','abc','new','description','123','abc'];
 export default class Body extends React.Component{
     
     render(){
