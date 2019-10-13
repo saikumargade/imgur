ef508bf77e2386df9418ed2009dde398165acd79
saikumargade <gade.saikumar1@gmail.com>
2019-10-13 13:44:27 +0530

    onclick color change for like and dislike icons

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -3,16 +3,33 @@ import logo from '../logo.svg'
 import '../App.css'
 import Icon from 'antd/es/icon'
 export default class Post extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            like:'',
+            dislike:'',
+        }
+    }
+    
+    handleLike=()=>{
+        this.newlike=this.state.like===''?'filled':'';
+        this.setState({like:this.newlike});
+    }
+     handleDislike=()=>{
+        this.newdislike=this.state.dislike===''?'filled':'';
+        this.setState({dislike:this.newdislike});
+    }
   
     render(){
-        const {title} = this.props
+        const {title} = this.props;
+        
         return(
             <div className="post">
                 <img style={{boxSizing:'border-box',borderRadius:'5px'}} width='100%' src={logo} alt="react logo"/>
                 <p style={{padding:'0px 10px'}}>{title}</p>
                 <div className='post-footer'>
-                <Icon type="like" style={{padding:'0px 15px'}} />
-                <Icon type="dislike" style={{padding:'0px 15px'}}/>
+                <Icon type="like"  theme={this.state.like} onClick={this.handleLike} style={{padding:'0px 15px',outline:'none'}} />
+                <Icon type="dislike"theme={this.state.dislike} onClick={this.handleDislike} style={{padding:'0px 15px',outline:'none'}}/>
                 {/* <div className='up-count'>0</div>
                 <div className='down-count'>0</div> */}
                 <Icon type='message' style={{padding:'0px 15px'}}/>
